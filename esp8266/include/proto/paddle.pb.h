ac4a1313c9850456d46e3b746f69e1918eaaacac
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2022-12-02 16:49:53 -0500

    clean up

--- esp8266/include/proto/paddle.pb.h
+++ esp8266/include/proto/paddle.pb.h
@@ -3,7 +3,7 @@
 
 #ifndef PB_BRICKBREAKER_PADDLE_PB_H_INCLUDED
 #define PB_BRICKBREAKER_PADDLE_PB_H_INCLUDED
-#include <pb.h>
+#include "pb/pb.h"
 
 #if PB_PROTO_HEADER_VERSION != 40
 #error Regenerate this file with the current version of nanopb generator.
